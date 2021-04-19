a78348867f439c48760f47eae780da727bf60483
Jon <jsmartware@aol.com>
2021-04-19 08:41:09 -0700

    Added Exception handling for production

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -47,6 +47,18 @@ namespace CourseLibrary.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+                        // TODO: log the fault as well
+                    });
+                });
+            }
 
             app.UseRouting();
 

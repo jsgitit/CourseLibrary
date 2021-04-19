6066bc1ab32cb832be1b6a62e0ca7d120673f091
Jon <jsmartware@aol.com>
2021-04-18 19:57:47 -0700

    Added Authors Profile custom field mappings

--- CourseLibrary.API/Profiles/AuthorsProfile.cs
+++ CourseLibrary.API/Profiles/AuthorsProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using CourseLibrary.API.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,15 @@ namespace CourseLibrary.API.Profiles
     {
         public AuthorsProfile()
         {
+            CreateMap<Entities.Author, Models.AuthorDTO>()
+                .ForMember(
+                dest => dest.Name,
+                opt =>
+                    opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ForMember(
+                dest => dest.Age,
+                opt =>
+                    opt.MapFrom(src => $"{src.DateOfBirth.GetCurrentAge()} {src.LastName}"));
 
         }
     }

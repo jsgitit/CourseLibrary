3a62b6cdda89b2efe86153cd49b8bcd27a1134ef
Jon <jsmartware@aol.com>
2021-04-20 15:02:24 -0700

    Added comment about FluentValidation

--- CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentThanDescriptionAttribute.cs
+++ CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentThanDescriptionAttribute.cs
@@ -6,7 +6,11 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace CourseLibrary.API.ValidationAttributes 
-{
+{/// <summary>
+/// This class contains an example of how to use a custom validation attribute to use on Creation DTOs
+/// However, a better approach to validation might be to use FluentValidation for complex rules, 
+/// because they are easier to test and separate the rules from the mdoels.
+/// </summary>
     public class CourseTitleMustBeDifferentThanDescriptionAttribute : ValidationAttribute
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)

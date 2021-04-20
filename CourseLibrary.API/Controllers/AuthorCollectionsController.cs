55dd5290c15c374531ade414280031c6a6eb6a38
Jon <jsmartware@aol.com>
2021-04-19 20:09:04 -0700

    Added new route to post multiple Authors in one batch

---
+++ CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorCollectionsController(
+            ICourseLibraryRepository courseLibraryRepository,
+            IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<AuthorDTO>> CreateAuthorCollection(
+            IEnumerable<AuthorForCreationDTO> authorCollection)
+        {
+            var authorEntities = 
+                _mapper.Map<IEnumerable<Entities.Author>>(authorCollection);
+
+            foreach (var author in authorEntities)
+            {
+                _courseLibraryRepository.AddAuthor(author);
+            }
+            _courseLibraryRepository.Save();
+
+            return Ok(); // TODO implement a Location URI
+        }
+    }
+}

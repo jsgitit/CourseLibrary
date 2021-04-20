1f57937bcc4934cd75750765dbb111d2d8fa84d6
Jon <jsmartware@aol.com>
2021-04-20 08:12:13 -0700

    Adding validation rules to match database entities to prevent null entries

--- CourseLibrary.API/Models/CourseForCreationDTO.cs
+++ CourseLibrary.API/Models/CourseForCreationDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,12 @@ namespace CourseLibrary.API.Models
 {
     public class CourseForCreationDTO
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+
+        [Required]
+        [MaxLength(1500)]
         public string Description { get; set; }
         
         //public Guid AuthorId { get; set; }  // removed, since authorId will be in the route template already

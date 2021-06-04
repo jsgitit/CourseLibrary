d91153f9d47da886c66785e75e3ac698ba59561f
Jon <jsmartware@aol.com>
2021-06-04 09:13:35 -0700

    added ResponseCaching() services in startup and configured it.

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -26,7 +26,7 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddResponseCaching();
             services.AddControllers(setupAction =>
             {
                 setupAction.ReturnHttpNotAcceptable = true; // restrict output formats
@@ -129,6 +129,7 @@ namespace CourseLibrary.API
                 });
             }
 
+            app.UseResponseCaching();
             app.UseRouting();
 
             app.UseAuthorization();
